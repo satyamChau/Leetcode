09d803a2a8f741e6a91c742c0180d34f3be4b9ee
satyamChau <113303682+satyamChau@users.noreply.github.com>
2023-05-10 20:27:35 +0530

    Added solution - LeetHub

--- Total Cuts - GFG/total-cuts.cpp
+++ Total Cuts - GFG/total-cuts.cpp
@@ -14,33 +14,35 @@ class Solution{
 public:
     int totalCuts(int N,int K,vector<int> &A){
         // Code here
-        // vector<int>ans(N,0);
-        // ans[N-1] = A[N-1];
-        // for(int i=N-2;i>=0; i--){
-        //     ans[i] = min(ans[i+1],A[i]);
-        // }
-        // int ans2 = A[0];
-        // int cnt=0;
-        // for(int i=1; i<N; i++){
-        //     if(ans2+ans[i]>=K){
-        //         cnt++;
-        //         ans2 = max(ans2,A[i]);
-        //     }
-        // }
-        // return cnt;
-         vector<int> pref(N, 0);
-        pref[N-1] = A[N-1];
-        for(int i = N-2; i>=0 ; i--){
-            pref[i] = min(pref[i+1],A[i]);
+        vector<int>ans(N,0);
+        ans[N-1] = A[N-1];
+        for(int i=N-2;i>=0; i--){
+            ans[i] = min(ans[i+1],A[i]);
         }
-        int maxi = A[0];
-        int ans = 0;
-        for(int i = 1; i < N; i++){
-            if(maxi + pref[i] >= K)
-                ans++;
-            maxi = max(maxi, A[i]);
+        int ans2 = A[0];
+        int cnt=0;
+        for(int i=1; i<N; i++){
+            if(ans2+ans[i]>=K){
+                cnt++;
+                
+            }
+        ans2 = max(ans2,A[i]);
+        
         }
-        return ans;
+        return cnt; 
+        // vector<int> pref(N, 0);
+        // pref[N-1] = A[N-1];
+        // for(int i = N-2; i>=0 ; i--){
+        //     pref[i] = min(pref[i+1],A[i]);
+        // }
+        // int maxi = A[0];
+        // int ans = 0;
+        // for(int i = 1; i < N; i++){
+        //     if(maxi + pref[i] >= K)
+        //         ans++;
+        //     maxi = max(maxi, A[i]);
+        // }
+        // return ans;
     }
 };
 
