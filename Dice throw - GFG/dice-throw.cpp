5f9f42679e994ab2099c0ad0cc1047b4f808b207
satyamChau <113303682+satyamChau@users.noreply.github.com>
2023-06-06 10:46:26 +0530

    Added solution - LeetHub

--- Dice throw - GFG/dice-throw.cpp
+++ Dice throw - GFG/dice-throw.cpp
@@ -7,26 +7,26 @@ class Solution {
   public:
   
     long long sol(int a, int b, int c){
-        vector<vector<long long>>dp(b+1, vector<long long>(c+1,0));
+        vector<vector<long long>>dp(a+1, vector<long long>(c+1,0));
         dp[0][0]=1;
         
        
-        for(int d=1;d<=b;d++){
+        for(int d=1;d<=a;d++){
             for(int e=1;e<=c;e++){
                  long long ans=0;
-                  for(int i=1; i<=a;i++){
+                  for(int i=1; i<=b;i++){
                     if(e-i>=0)
                       ans= ans + dp[d-1][e-i];
                 }
                 dp[d][e]=ans;
             }
         }
-        return dp[b][c];
+        return dp[a][c];
     }
     long long noOfWays(int M , int N , int X) {
         // code here 
         
-        return sol(M,N,X);
+        return sol(N,M,X);
         //   vector<long long> dp(X + 1 , 0);
         //   dp[0] = 1;
         
