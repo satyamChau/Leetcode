c1a6d0af41aed9c3ddaf9d1c967001ba26a23d63
satyamChau <113303682+satyamChau@users.noreply.github.com>
2023-06-06 10:42:47 +0530

    Added solution - LeetHub

--- Dice throw - GFG/dice-throw.cpp
+++ Dice throw - GFG/dice-throw.cpp
@@ -6,45 +6,45 @@ using namespace std;
 class Solution {
   public:
   
-    // long long sol(int a, int b, int c){
-    //     vector<vector<long long>>dp(a+1, vector<long long>(c+1,0));
-    //     dp[0][0]=1;
+    long long sol(int a, int b, int c){
+        vector<vector<long long>>dp(b+1, vector<long long>(c+1,0));
+        dp[0][0]=1;
         
        
-    //     for(int d=1;d<=a;d++){
-    //         for(int e=1;e<=c;e++){
-    //              long long ans=0;
-    //               for(int i=1; i<=b;i++){
-    //                 if(e-i>=0)
-    //                   ans= ans + dp[d-1][e-i];
-    //             }
-    //             dp[d][e]=ans;
-    //         }
-    //     }
-    //     return dp[a][c];
-    // }
+        for(int d=1;d<=b;d++){
+            for(int e=1;e<=c;e++){
+                 long long ans=0;
+                  for(int i=1; i<=a;i++){
+                    if(e-i>=0)
+                      ans= ans + dp[d-1][e-i];
+                }
+                dp[d][e]=ans;
+            }
+        }
+        return dp[b][c];
+    }
     long long noOfWays(int M , int N , int X) {
         // code here 
         
-        // return sol(M,N,X);
-          vector<long long> dp(X + 1 , 0);
-          dp[0] = 1;
+        return sol(M,N,X);
+        //   vector<long long> dp(X + 1 , 0);
+        //   dp[0] = 1;
         
-        for(int i = 1 ; i <= N ; i++){
-            for(int j = X ; j >= 0 ; j--){
-                long long cnt = 0;
+        // for(int i = 1 ; i <= N ; i++){
+        //     for(int j = X ; j >= 0 ; j--){
+        //         long long cnt = 0;
         
-                for(int k = 1 ; k <= M ; k++){
-                    if(j - k >= 0){
-                        cnt += dp[j - k];
-                    }
-                }
+        //         for(int k = 1 ; k <= M ; k++){
+        //             if(j - k >= 0){
+        //                 cnt += dp[j - k];
+        //             }
+        //         }
                 
-                dp[j] = cnt;
-            }
-        }
+        //         dp[j] = cnt;
+        //     }
+        // }
         
-        return dp[X];
+        // return dp[X];
     }
 };
 
