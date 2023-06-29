c2cccba48eb158aa545d6549ebf4645a7b10935d
satyamChau <113303682+satyamChau@users.noreply.github.com>
2023-06-29 20:01:47 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -7,46 +7,36 @@ using namespace std;
 class Solution{
     
 private:
-    // int binarysearch(vector<int> &arr, int n) {
-    //     int s=0 , e = n-1;
-    //     int ans=-1;
-    //     while(s<=e){
-    //         int mid = s + (e-s)/2;
-    //         if(arr[mid] == 1){
-    //             ans = mid;
-    //             e = mid-1;
-    //         }
-    //         else{
-    //             s = mid+1;
-    //         }
-    //     }
-    //     return ans;
-    // }
+    int binarysearch(vector<int> &arr, int n) {
+        int s=0 , e = n-1;
+        int ans=-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid] == 1){
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return ans;
+    }
 public:
 
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
 	    // code here
-	   // int ans = -1;
-	   // int mx=-1;
-	   // for(int i=0; i<n; i++){
-	   //    int index = binarysearch(arr[i], m);
-	   //     if(index != -1 && m-index > mx){
-	   //         mx = m-index ;
-	   //         ans = i;
-	   //     }
-	   // }
-	   // return ans;
-	   int row = -1, j = m-1;
-        for(int i = 0; i<n; i++)
-        {
-            while(j>=0 && arr[i][j] == 1)
-            {
-                j--;
-                row = i;
-            }
-        }
-        return row;
-	            
+	    int ans = -1;
+	    int mx=-1;
+	    for(int i=0; i<n; i++){
+	       int index = binarysearch(arr[i], m);
+	        if(index != -1 && m-index > mx){
+	            mx = m-index ;
+	            ans = i;
+	        }
+	    }
+	    return ans;
+	  
 	        
 	    }
 
