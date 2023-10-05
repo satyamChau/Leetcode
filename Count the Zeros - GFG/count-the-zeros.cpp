02400f7f5b3de474dc74abbd7222f26f5263f9ae
satyamChau <113303682+satyamChau@users.noreply.github.com>
2023-10-05 16:20:32 +0530

    Added solution - LeetHub

---
+++ Count the Zeros - GFG/count-the-zeros.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+         int low = 0, high = n-1, mid;
+        while(low <= high)  {
+            mid = (low+high)/2;
+            if(arr[mid] == 0)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return n-1-high;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
